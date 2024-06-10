1c05c8ce70920f2144bd07ed25e74b6fe24b52a0
devashishHp04 <devashish04.hp@gmail.com>
2024-06-10 22:42:52 +0530

    hello

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/auth-context";
 import { Toaster } from "sonner";
-import GoogleTagManager from "@/components/custom/GoogleTagManager";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,12 +20,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <html>
-        <GoogleTagManager/>
+        <GoogleTagManager gtmId="GTM-KN93RKTH" />
       </html>
       <body className={inter.className} >
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KN93RKTH"
+            height="0" width="0" style={{display:"none" , visibility:"hidden"}}>
+            </iframe>
+        </noscript>
         <AuthProvider>
           {children}
-          <Toaster position="top-right"/>
+          <Toaster position="top-right" />
         </AuthProvider>
       </body>
     </html>
