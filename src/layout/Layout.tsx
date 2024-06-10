2cf75c143f508844af8d50637b519d7534f58245
devashishHp04 <devashish04.hp@gmail.com>
2024-06-10 21:41:30 +0530

    sadf

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,9 +1,7 @@
-import Navbar from '@/components/Navbar'
 import React from 'react'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (<>
-        <Navbar />
         {children}
     </>
     )
